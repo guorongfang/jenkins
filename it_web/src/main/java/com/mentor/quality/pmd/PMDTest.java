b1dab67fe36d030223ce197ff38c22f6291e2fcb
fang101 <gfang@frontier.com>
2015-09-26 00:21:08 -0700

    initial commit

---
+++ it_web/src/main/java/com/mentor/quality/pmd/PMDTest.java
@@ -0,0 +1,17 @@
+package com.mentor.quality.pmd;
+
+//PMD Eclipse Tutorial
+public class PMDTest {
+	public static void main(String args[]) {
+		PMDTest.CALL_METHOD("hello");
+		PMDTest.CallHello();
+	}
+
+	public static void CALL_METHOD(String INPUT_PARAMETER) {
+		System.out.println(INPUT_PARAMETER);
+	}
+
+	public static void CallHello() {
+		System.out.println("Hello PMD World!");
+	}
+}
