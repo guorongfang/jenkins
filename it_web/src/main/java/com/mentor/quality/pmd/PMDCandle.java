1616b3764b9cb6ec32e3dbcc41c6162a3b2dd2da
Guorong Fang <guorong_fang@mentor.com>
2015-09-28 16:26:55 -0700

    initial load

---
+++ it_web/src/main/java/com/mentor/quality/pmd/PMDCandle.java
@@ -0,0 +1,17 @@
+package com.mentor.quality.pmd;
+
+import java.util.Date;
+
+public class PMDCandle {
+	private String MyIP = "123.123.123.123";
+	
+	public void dontDontDoThisInYoourCode(){	
+		System.out.println("Logging Framework please");
+		try {
+			int x =5;	
+		}catch(Exception e){}
+		String myString=null;
+		if (myString.contentEquals("NPE here"));		
+	}
+
+}
