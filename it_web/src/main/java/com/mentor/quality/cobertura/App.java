6a5d506b0454f25cfccb9da904e993cd027d4fbf
Guorong Fang <guorong_fang@mentor.com>
2015-10-07 09:23:51 -0700

    fixed

---
+++ it_web/src/main/java/com/mentor/quality/cobertura/App.java
@@ -0,0 +1,14 @@
+package com.mentor.quality.cobertura;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
