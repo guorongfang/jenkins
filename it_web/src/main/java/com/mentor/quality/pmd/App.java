8e95f894d2c0776a97f6264f36721d764aff7c1c
Guorong Fang <guorong_fang@mentor.com>
2015-10-07 09:21:44 -0700

    update with bug

---
+++ it_web/src/main/java/com/mentor/quality/pmd/App.java
@@ -0,0 +1,17 @@
+package com.mentor.quality.pmd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        String PASSWORD="secret";
+        
+        blar;
+        
+     }
+}
